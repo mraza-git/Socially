988ecf87ed4b294e23b2faa9c53e81ffb1932dd4
ansaries <arslan@ansaries.com>
2016-10-18 12:40:24 +0400

    Delete environment.js

--- server/startup/environment.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  process.env.DDP_DEFAULT_CONNECTION_URL = "http://192.168.99.153:3005"
-  process.env.ROOT_URL = "http://192.168.99.153:3005"
-})
-;
